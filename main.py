3f73e9948fc96ddc93f653f5fcccc6ff7d4b29fa
danirguez <32953186+danirguez@users.noreply.github.com>
2024-11-03 12:57:52 +0100

    Dockerfile añadido a main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import dice
-        from time import sleep
+from time import sleep
 
-        def roll(amount:int, sides:int):
-            return dice.roll(f'{amount}d{sides}')
+def roll(amount:int, sides:int):
+    return dice.roll(f'{amount}d{sides}')
 
-        for idx, result in enumerate(roll(6,6)):
-            print(f'Lanzamiento {idx+1} número obtenido {result}')
-            sleep(5)
+for idx, result in enumerate(roll(6,6)):
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
+    sleep(5)
