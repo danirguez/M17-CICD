29caa8f67ce5cd37853430e8f3c131c9874e3504
danirguez <32953186+danirguez@users.noreply.github.com>
2024-11-03 12:28:06 +0100

    Revert "Tercer commit: Modificado el mensaje de salida"
    This reverts commit ea16e8d05eb0866783c12a7b67f620d45826981a.

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import dice
             return dice.roll(f'{amount}d{sides}')
 
         for idx, result in enumerate(roll(5,6)):
-            print(f'Resultado del lanzamiento {idx+1}: {result}')
+            print(f'Lanzamiento {idx+1} número obtenido {result}')
             sleep(5)
